3d83ba0cb8d3efc0e86aabec578ccaab91c791c1
IhorKozloff <87847488+IhorKozloff@users.noreply.github.com>
2023-11-01 15:45:46 +0200

    loader added to home page

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -1,12 +1,13 @@
+import { Hourglass } from 'react-loader-spinner';
 import { NewsList } from '../components/NewsList';
 import { useGetNewsGeneralInfoQuery } from '../store/api/newsApi';
 
 export const HomePage = () => {
-    const { data } = useGetNewsGeneralInfoQuery();
+    const { data, isLoading } = useGetNewsGeneralInfoQuery();
  
     return (
         <>
-
+            {isLoading && <div className="fixed top-0 right-0 left-0 bottom-0 flex items-center justify-center"><Hourglass/></div>}
             {data && <NewsList newsData={data} />}
         </>
     );
