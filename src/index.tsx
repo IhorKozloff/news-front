dd3a3af84ed4094edcdaa32e069e14e9af246b14
IhorKozloff <87847488+IhorKozloff@users.noreply.github.com>
2023-11-01 11:28:53 +0200

    gh-pages settings

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import {App} from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store, persistor } from './store';
 import { PersistGate } from 'redux-persist/integration/react';
@@ -13,9 +13,9 @@ const root = ReactDOM.createRoot(
 root.render(
     <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
-            <BrowserRouter basename="news">
+            <HashRouter basename="/news">
                 <App />
-            </BrowserRouter>
+            </HashRouter>
         </PersistGate>
     </Provider> 
 );
